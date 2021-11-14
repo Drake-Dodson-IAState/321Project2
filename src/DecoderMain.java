1a242df633bc372b63a9630065dd35b313e213bc
Drake Dodson <drwdodson@gmail.com>
2021-11-14 14:44:38 -0600

    removed some misleading comments

--- src/DecoderMain.java
+++ src/DecoderMain.java
@@ -7,19 +7,23 @@ public class DecoderMain {
             System.exit(0);
         }
 
-        System.out.println("Hello?");
         // We'll need to create an outfile of some sort
         String inFile = args[0];
+        String outFile = "assembly.legv8asm";
 
-        try(InputStream inputStream = new FileInputStream(inFile);)
-        {
+        try (
+            InputStream inputStream = new FileInputStream(inFile);
+            OutputStream outputStream = new FileOutputStream(outFile);
+        ) {
             int byteRead = inputStream.read();
             String s1 = String.format("%8s", Integer.toBinaryString(byteRead & 0xFF)).replace(' ', '0');
-            System.out.println(s1); // 10000001
+            System.out.println(s1);
         }
         catch (IOException ex) {
             ex.printStackTrace();
         }
+
+
     }
 }
 
