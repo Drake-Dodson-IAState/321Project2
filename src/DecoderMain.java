ffa9086621202757ed95bc47ee693790041795fd
Drake Dodson <drwdodson@gmail.com>
2021-11-14 20:44:39 -0600

    The code now determines what opcode the instruction is

--- src/DecoderMain.java
+++ src/DecoderMain.java
@@ -60,13 +60,16 @@ public class DecoderMain {
     }
 
     public static void determineOpCode(int instructionWord) {
+        // 6 bits
         if((instructionWord & 0xFC000000) == 0x14000000){
             // Branch
             System.out.println("Branch");
         } else if ((instructionWord & 0xFC000000) == 0x94000000){
             System.out.println("Branch Link");
             // Branch Link
-        } else if ((instructionWord & 0xFF000000) == 0x54000000) {
+        }
+        // 8 bits
+        else if ((instructionWord & 0xFF000000) == 0x54000000) {
             System.out.println("Branch Conditional");
             // Branch Conditional
         } else if ((instructionWord & 0xFF000000) == 0xB5000000) {
@@ -74,10 +77,79 @@ public class DecoderMain {
             // Compare Not Zero
         } else if ((instructionWord & 0xFF000000) == 0xB4000000) {
             System.out.println("Compare Zero");
-            // Compare Not Zero
-        } else if ((instructionWord & 0xFFE00000) == 0x8B000000) {
+            // Compare Zero
+        }
+        // 10 bits
+        else if ((instructionWord & 0xFFC00000) == 0x91000000) {
+            System.out.println("Add I");
+            // Add Immediate
+        } else if ((instructionWord & 0xFFC00000) == 0x92000000) {
+            System.out.println("And I");
+            // And Immediate
+        } else if ((instructionWord & 0xFFC00000) == 0xD2000000) {
+            System.out.println("Exclusive Or I");
+            // Exclusive Or Immediate
+        } else if ((instructionWord & 0xFFC00000) == 0xB2000000) {
+            System.out.println("Orr I");
+            // Orr Immediate
+        } else if ((instructionWord & 0xFFC00000) == 0xD1000000) {
+            System.out.println("Subtract I");
+            // Sub Immediate
+        }
+
+
+        // 11 bits
+        else if ((instructionWord & 0xFFE00000) == 0x8B000000) {
             System.out.println("Add");
-            // Compare Not Zero
+            // Add
+        } else if ((instructionWord & 0xFFE00000) == 0x8A000000) {
+            System.out.println("And");
+            // And
+        } else if ((instructionWord & 0xFFE00000) == 0xD6000000) {
+            System.out.println("Branch Register");
+            // Branch to register
+        }  else if ((instructionWord & 0xFFE00000) == 0xCA000000) {
+            System.out.println("Exclusive Or");
+            // Exclusive Or
+        } else if ((instructionWord & 0xFFE00000) == 0xF8400000) {
+            System.out.println("Load Register");
+            // Load Register
+        } else if ((instructionWord & 0xFFE00000) == 0xF8000000) {
+            System.out.println("Store Register");
+            // Store
+        } else if ((instructionWord & 0xFFE00000) == 0xD3600000) {
+            System.out.println("Logical Shift Left");
+            // Logical Shift left
+        } else if ((instructionWord & 0xFFE00000) == 0xD3400000) {
+            System.out.println("Logical Shift Right");
+            // Logical Shift left
+        } else if ((instructionWord & 0xFFE00000) == 0xAA000000) {
+            System.out.println("Orr");
+            // Orr
+        } else if ((instructionWord & 0xFFE00000) == 0xCB000000) {
+            System.out.println("Subtract");
+            // Subtract
+        } else if ((instructionWord & 0xFFE00000) == 0xF1000000) {
+            System.out.println("Subtract Immediate and Set");
+            // Subtract Immediate and Set
+        } else if ((instructionWord & 0xFFE00000) == 0xEB000000) {
+            System.out.println("Subtract and Set");
+            // Subtract and Set
+        } else if ((instructionWord & 0xFFE00000) == 0x9B000000) {
+            System.out.println("Multiply");
+            // Multiply
+        } else if ((instructionWord & 0xFFE00000) == 0xFFC00000) {
+            System.out.println("DUMP");
+            // Dump
+        } else if ((instructionWord & 0xFFE00000) == 0xFFE00000) {
+            System.out.println("HALT");
+            // Halt
+        } else if ((instructionWord & 0xFFE00000) == 0xFF800000) {
+            System.out.println("Prnl");
+            // Prnl
+        } else if ((instructionWord & 0xFFE00000) == 0xFFA00000) {
+            System.out.println("Prnt");
+            // Prnt
         } else {
             System.out.println("N/A");
         }
